3214fe30687ce1b34867255685d7e81e8a311a67
José L. Patiño <jose.lpa@gmail.com>
2015-05-21 19:29:24 +0100

    Starting...

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='elastic-django',
+    version='0.1',
+    description='Python Distribution Utilities',
+    author='Jose L. Patino Andres',
+    author_email='jose.lpa@gmail.com',
+    url='http://patino.me',
+    packages=['elasticsearch'],
+)
