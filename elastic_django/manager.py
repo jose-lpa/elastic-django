534df73886a6b3a6e66ad55e12ff9b0b3a8bda4b
José L. Patiño <jose.lpa@gmail.com>
2015-05-25 19:47:24 +0100

    Handling unavailable ES backend in `ElasticManager` - it should inform and pass.

--- elastic_django/manager.py
+++ elastic_django/manager.py
@@ -1,6 +1,19 @@
+import logging
+
+from django.conf import settings
+
 from .client import ElasticsearchClient
+from .exceptions import ElasticsearchClientConfigurationError
+
+
+# logger = getLogger('elastic_django')
 
 
 class ElasticManager(object):
     def __init__(self):
-        self.client = ElasticsearchClient()
+        try:
+            self.client = ElasticsearchClient()
+        except ElasticsearchClientConfigurationError:
+            message = 'Elasticsearch connection unavailable.'
+            logging.exception(message) if settings.DEBUG else logging.critical(
+                message)
