a391d3233fe03126d871249b9b397ec9e227f0e6
José L. Patiño <jose.lpa@gmail.com>
2015-05-25 14:58:20 +0100

    Minimum ES client configured.

--- elastic_django/client.py
+++ elastic_django/client.py
@@ -12,35 +12,36 @@ from django.conf import settings
 from elasticsearch import Elasticsearch
 from elasticsearch.transport import Transport
 
-class ElasticsearchClientConfigurationError(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-        message = 'Invalid client configuration: {0}'.format(self.value)
-        return repr(message)
+from .exceptions import ElasticsearchClientConfigurationError
 
 
 class ElasticsearchClient(object):
     def __init__(self, hosts=None, transport_class=None, **kwargs):
         # Sanity checks.
-        try:
-            assert isinstance(hosts, (list, tuple))
-        except AssertionError as e:
-            raise ElasticsearchClientConfigurationError(e)
-
-        auto_host = None
-        if not hosts:
+        if hosts:
+            try:
+                assert isinstance(hosts, (list, tuple))
+                self.hosts = hosts
+            except AssertionError as e:
+                raise ElasticsearchClientConfigurationError(e)
+        else:
+            # No hosts given. Automatic configuration to default ES node.
             auto_host = getattr(
                 settings, 'ELASTICSEARCH_HOSTS', 'http://localhost:9200')
-
             auto_host = urlparse(auto_host)
-
-        self.hosts = hosts or [
-            {'host': auto_host.hostname, 'port': auto_host.port}]
+            self.hosts = [{'host': auto_host.hostname, 'port': auto_host.port}]
 
         if not transport_class:
             self.transport = Transport
 
         self.connection = Elasticsearch(
             hosts=self.hosts, transport_class=self.transport, **kwargs)
+
+        # Check connection before continuing.
+        if not self.connection.ping():
+            raise ElasticsearchClientConfigurationError(
+                'Elasticsearch backend unreachable. Check host configuration.')
+        else:
+            logging.debug(
+                "Connection established with Elasticsearch backend(s) in "
+                "'{0}'".format(self.connection.transport.hosts))
