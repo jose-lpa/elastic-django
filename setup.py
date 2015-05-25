7f785b00828b4bc473c8ff28b27e55fd375db090
José L. Patiño <jose.lpa@gmail.com>
2015-05-25 17:41:16 +0100

    Now using PyTest to test the project.

--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
+import sys
 
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Import here, cause outside the eggs aren't loaded.
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
 setup(
@@ -13,5 +34,11 @@ setup(
     packages=find_packages(),
     install_requires=['elasticsearch', 'Django>=1.8'],
     test_suite='tests',
-    tests_require=['mock']
+    tests_require=[
+        'mock',
+        'pytest',
+        'pytest-cov',
+        'pytest-pep8',
+    ],
+    cmdclass={'test': PyTest},
 )
