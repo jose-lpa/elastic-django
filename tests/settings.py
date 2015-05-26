d0dfd956499716e3a16f0eb51bf03c2109fa6a75
José L. Patiño <jose.lpa@gmail.com>
2015-05-26 19:23:55 +0100

    Added a couple more options to base settings.

--- tests/settings.py
+++ tests/settings.py
@@ -2,3 +2,5 @@ SECRET_KEY = 'Elasticsearch meets Django'
 
 ELASTICSEARCH_HOSTS = [{'host': 'localhost', 'port': '9200'}]
 ELASTICSEARCH_TRANSPORT_CLASS = None
+ELASTICSEARCH_INDEX_NAME = 'testing-elasticdjango'
+ELASTICSEARCH_AUTO_INDEX = True
