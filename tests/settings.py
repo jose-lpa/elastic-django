a4528af68462f454c3dc4c5afb924a0312761225
José L. Patiño <jose.lpa@gmail.com>
2015-05-28 21:45:28 +0100

    DB backend for Django models enabled in tests.

--- tests/settings.py
+++ tests/settings.py
@@ -1,5 +1,15 @@
 SECRET_KEY = 'Elasticsearch meets Django'
 
+DATABASES = {
+    'default': {
+        # Memory resident database, for easy testing.
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+INSTALLED_APPS = ['tests']
+
+# Elastic-Django custom settings.
 ELASTICSEARCH_HOSTS = [{'host': 'localhost', 'port': '9200'}]
 ELASTICSEARCH_TRANSPORT_CLASS = None
 ELASTICSEARCH_INDEX_NAME = 'testing-elasticdjango'
