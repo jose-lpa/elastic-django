39c029d8849630554a3f09b16e626edce2c187c1
José L. Patiño <jose.lpa@gmail.com>
2015-05-31 17:19:51 +0100

    Added test models for specific test cases.

--- tests/models.py
+++ tests/models.py
@@ -4,6 +4,20 @@ from elastic_django.models import ElasticModel
 
 
 class Book(ElasticModel):
+    """
+    Main 'Book' model.
+    """
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, null=True, blank=True)
+    publication_year = models.SmallIntegerField()
+    description = models.TextField(blank=True)
+
+
+class BookSelection(ElasticModel):
+    """
+    'Book' model with a selection of fields to be indexed.
+    """
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13, null=True, blank=True)
@@ -12,3 +26,17 @@ class Book(ElasticModel):
 
     class Meta:
         elastic_fields = ('title', 'author')
+
+
+class BookExclusion(ElasticModel):
+    """
+    'Book' model with a selection of fields to be excluded from index.
+    """
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, null=True, blank=True)
+    publication_year = models.SmallIntegerField()
+    description = models.TextField(blank=True)
+
+    class Meta:
+        elastic_exclude = ('title', 'author')
