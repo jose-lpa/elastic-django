08fd9a3282d29d260855ad705f41ead36ed7d251
José L. Patiño <jose.lpa@gmail.com>
2015-06-03 17:58:11 +0100

    Fixed several things in `drop_index` management command.

--- elastic_django/management/commands/drop_index.py
+++ elastic_django/management/commands/drop_index.py
@@ -8,13 +8,14 @@ from elasticsearch.exceptions import NotFoundError
 from ...client import ElasticsearchClient
 from ...exceptions import ElasticsearchClientConfigurationError
 
+
 class Command(BaseCommand):
     help = 'Entirely removes a search index from Elasticsearch backend. USE ' \
            'IT WITH CAUTION.'
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'index_name', nargs=1,
+            'index_name', nargs='?',
             help='Name of the Elasticsearch index to be removed.')
 
     def handle(self, *args, **options):
@@ -32,14 +33,30 @@ class Command(BaseCommand):
                     'No Elasticsearch index name defined and no autoconfigured'
                     ' indices found to remove. Please specify an index name.')
 
-        try:
-            response = client.connection.indices.delete(index_name)
-            if response.get('acknowledged', False):
-                self.stdout.write("Dropped Elasticsearch index '{0}'".format(
-                    index_name))
-            else:
-                self.stdout.write("Something occurred. Elasticsearch index "
-                                  "'{0}' was not dropped.".format(index_name))
-        except NotFoundError:
-            raise CommandError("No index called '{0}' found in Elasticsearch "
-                               "backend.".format(index_name))
+        msg = "\nYou have chosen to entirely remove the index '{0}' in the " \
+              "Elasticsearch backend.\nThis operation cannot be undone. " \
+              "Confirm? (yes/no)\n".format(index_name)
+        confirm = raw_input(msg)
+        while 1:
+            if confirm not in ('yes', 'no'):
+                confirm = raw_input("Please enter either 'yes' or 'no': ")
+                continue
+            if confirm == 'yes':
+                try:
+                    response = client.connection.indices.delete(index_name)
+                    if response.get('acknowledged', False):
+                        self.stdout.write(
+                            "\nDropped Elasticsearch index '{0}'".format(
+                                index_name))
+                    else:
+                        self.stdout.write(
+                            "Something occurred. Elasticsearch index '{0}' was"
+                            " not dropped.".format(index_name))
+                except NotFoundError:
+                    raise CommandError(
+                        "No index called '{0}' found in Elasticsearch "
+                        "backend.".format(index_name))
+            if confirm == 'no':
+                self.stdout.write("\nGiving up. No Elasticsearch index was "
+                                  "harmed during this operation.\n")
+            break
