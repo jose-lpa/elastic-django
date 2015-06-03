49f37696f1905f49262c0e6e44dfabd3015b17aa
José L. Patiño <jose.lpa@gmail.com>
2015-06-03 11:17:08 +0100

    Added `coveralls` egg to setup testing script.

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=['elasticsearch', 'Django>=1.8'],
     test_suite='tests',
     tests_require=[
+        'coveralls',
         'mock',
         'pytest-django',
         'pytest-cov',
